0ce79ba1c532a7d6e74596a3ecf0b94725cc4d98
D <D@Ds-MacBook-Air.local>
2026-03-06 08:14:03 +0530

    rewrite chatbot fixed imports

--- chatbot.py
+++ chatbot.py
@@ -5,33 +5,27 @@ from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_groq import ChatGroq
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.chains import RetrievalQA
+from langchain_community.chains import RetrievalQA
 
 load_dotenv()
 
 def load_knowledge_base():
     loader = TextLoader("restaurant_data.txt")
     documents = loader.load()
-    
     splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     chunks = splitter.split_documents(documents)
-    
     embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
     vectorstore = Chroma.from_documents(chunks, embeddings)
-    
     return vectorstore
 
 def create_chatbot(vectorstore):
     llm = ChatGroq(model="llama-3.3-70b-versatile", temperature=0)
-    
     retriever = vectorstore.as_retriever()
-    
     chatbot = RetrievalQA.from_chain_type(
         llm=llm,
         chain_type="stuff",
         retriever=retriever
     )
-    
     return chatbot
 
 def ask(chatbot, question):
