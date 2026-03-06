845beeac968170c65b0df70c2d24f39e18ba15ef
D <D@Ds-MacBook-Air.local>
2026-03-06 08:28:22 +0530

    fix all imports to langchain_core

--- chatbot.py
+++ chatbot.py
@@ -5,7 +5,7 @@ from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_groq import ChatGroq
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 from langchain_core.output_parsers import StrOutputParser
 
